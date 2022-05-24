7d31c459e186a036ffa4d68f24aeb458271a6d91
duongital <duongital@gmail.com>
2022-05-24 11:29:19 +0700

    feat: add animal

---
+++ prisma/migrations/20220524042806_add_animal/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "animal" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+
+    CONSTRAINT "animal_pkey" PRIMARY KEY ("id")
+);
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,9 +3,9 @@ import useSwr from 'swr'
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function About() {
-  const { data, error } = useSwr('/api/images', fetcher)
+  const { data, error } = useSwr('/api/animal', fetcher)
 
-  if (error) return <div>Failed to load images</div>
+  if (error) return <div>Failed to load animal</div>
   if (!data) return <div>Loading...</div>
 
   return (

35f533b5c02ae1d1aaaddea6accf3df21212889f
duongital <duongital@gmail.com>
2022-05-24 10:57:33 +0700

    chore: debug production

--- src/pages/api/images.js
+++ src/pages/api/images.js
@@ -1,6 +1,6 @@
 import prisma from 'lib/prisma'
 
 export default async function handle(req, res) {
-  const result = await prisma.images.findMany()
+  const result = await prisma.todo.findMany()
   res.json(result)
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,7 +5,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export default function About() {
   const { data, error } = useSwr('/api/images', fetcher)
 
-  if (error) return <div>Failed to load user</div>
+  if (error) return <div>Failed to load images</div>
   if (!data) return <div>Loading...</div>
 
   return (
